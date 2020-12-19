c2e7f66eed5c448fe63644973ab695c88b689f17
Kishor82 <kishorrathva8298@gmail.com>
2020-12-19 22:25:09 +0530

    [F-17] Added edit screen

---
+++ src/screens/UserEditScreen.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState, FunctionComponent } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { getUserDetails } from '../actions/userActions';
+
+interface Props {}
+
+const UserEditScreen: FunctionComponent<Props> = () => {
+  const { id: userId } = useParams<any>();
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [name, setName] = useState('');
+
+  const dispatch = useDispatch();
+
+  const { loading, error, user } = useSelector((state: any) => state.userDetails);
+
+  useEffect(() => {
+    if (!user.name || user._id !== userId) {
+      dispatch(getUserDetails(userId));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user, userId, dispatch]);
+
+  const submitHandler = (e: React.FormEvent<any>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name">
+              <Form.Label>name Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e: React.ChangeEvent<any>) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e: React.ChangeEvent<any>) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="isadmin">
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(e: React.ChangeEvent<any>) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default UserEditScreen;
