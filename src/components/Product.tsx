15e6bf29a3e5cbf426c865d6362b857072a4b5aa
Kishor82 <kishorrathva8298@gmail.com>
2020-12-19 22:23:55 +0530

    [F-02] Added Home screen component for product listing.

---
+++ src/components/Product.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import Rating from './Rating';
+interface Props {
+  product: any;
+}
+
+const Product = ({ product }: Props) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} color="#5aa962" />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
