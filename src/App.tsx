e4c61c41ced2dd3c4c484b2ac28e87d0344fc6af
Kishor82 <kishorrathva8298@gmail.com>
2020-12-19 22:24:22 +0530

    [F-03] Added Rating component for products.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Container } from 'react-bootstrap';
-import './App.css';
-
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome </h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ src/components/Rating.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Props {
+  value: number;
+  text: string;
+  color?: string;
+}
+
+const Rating = ({ value, text, color }: Props) => {
+  return (
+    <div className="rating">
+      <span>
+        <i style={{ color }} className={value >= 1 ? 'fas fa-star' : value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
+        <i style={{ color }} className={value >= 2 ? 'fas fa-star' : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
+        <i style={{ color }} className={value >= 3 ? 'fas fa-star' : value >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
+        <i style={{ color }} className={value >= 4 ? 'fas fa-star' : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
+        <i style={{ color }} className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'} />
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: '#f8e825',
+};
+export default Rating;
