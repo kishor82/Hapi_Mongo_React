a8406dcbeb892d8b1de263964d31b24ab4e56148
Kishor82 <kishorrathva8298@gmail.com>
2020-12-20 08:05:06 +0530

    [F-19] Added product screen.

---
+++ src/screens/ProductListScreen.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, FunctionComponent } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Col, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions';
+
+interface Props {}
+
+const ProductListScreen: FunctionComponent<Props> = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const { loading, error, products } = useSelector((state: any) => state.productList);
+
+  const { userInfo } = useSelector((state: any) => state.userLogin);
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history, userInfo]);
+
+  const deleteHandler = (id: string) => {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this product!',
+      icon: 'warning',
+      buttons: [true, true],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        // dispatch(deleteProduct(id));
+        swal('Product has been deleted!', {
+          icon: 'success',
+        });
+      }
+    });
+  };
+
+  const createProductHandler = (product: any) => {};
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-right">
+          <Button className="my-3" onClick={createProductHandler}>
+            <i className="fas fa-plus"></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product: any) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit"></i>
+                    </Button>
+                  </LinkContainer>
+                  <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(product._id)}>
+                    <i className="fas fa-trash"></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default ProductListScreen;
